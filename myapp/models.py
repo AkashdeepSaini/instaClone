46c6a9c09fde1c7d7d0d23769f1ef8fad1685c6b
AkashdeepSaini <akashdeepsaini05@gmail.com>
2017-07-18 21:55:47 +0530

    instaclone

--- myapp/models.py
+++ myapp/models.py
@@ -5,8 +5,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=30)
-    age = models.IntegerField(default=0)
-    has_verified_mobile = models.BooleanField(default=False)
+    email = models.EmailField()
+    name = models.CharField(max_length=120)
+    username = models.CharField(max_length=120)
+    password = models.CharField(max_length=40)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
